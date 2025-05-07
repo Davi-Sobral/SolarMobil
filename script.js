d255f95cebf230e724dfd8e55d426f6eb6af03ce
Davi-Sobral <davissobral300909@gmail.com>
2025-05-07 17:28:30 -0300

    Versão 1.02

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let btn  = document.querySelector("#inbotao");
 let imagem = document.querySelector("#infoto");
+let texto = document.querySelector("#intexto");
 imagem.style.display = "none";
 
-btn.addEventListener("click", (e)=>{
-   if(imagem.style.display=== "none"){
+
+btn.addEventListener("click", ()=>{
+   if(imagem.style.display === "none"){
    imagem.style.display = "flex";
+   texto.innerHTML = "";
 }
 else
    {
       imagem.style.display= "none";
+      texto.innerHTML = "Bem vindo ao projeto SolarMobil";
    }
 })
